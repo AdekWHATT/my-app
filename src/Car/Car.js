fcc4fdc23765edb1b09dbc32182cf0661f910e50
AdekWHATT <vyacheslaw.kilin@mail.ru>
2021-09-13 21:41:35 +0300

    5.4 Stateful Car.js and Car component

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,48 +1,53 @@
 import './Car.css'
 import Radium from 'radium'
+import React from 'react'
 
 
-const Car = (props) => { 
-    const inputClasses = ['input']
-    if (props.name !== '') {
-        inputClasses.push('green') 
-    } else {
-        inputClasses.push('red')
-    }
-
-    if (props.name.length > 4) {
-        inputClasses.push('bold')
-    }
-
-    const style = {
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-        ':hover': {
-            border: '1px solid #aaa',
-            boxShadow: '0 4px 15px 0 rgba(0,0,0, .25',
-            cursos: 'pointer'
+class Car extends React.Component {
+   
+    render() {
+      
+        const inputClasses = ['input']
+        if (this.props.name !== '') {
+            inputClasses.push('green') 
+        } else {
+            inputClasses.push('red')
         }
-    }
-
-
-return (
-<div className="Car" style={style} >
-    <h3>Car Name: {props.name}</h3>
-    <p>Year: <strong>{props.year}</strong></p>
     
-    <input 
-    type="text " 
-    onChange={props.onChangeName} 
-    value={props.name}
-    className={inputClasses.join(' ')}
-    />
+        if (this.props.name.length > 4) {
+            inputClasses.push('bold')
+        }
+    
+        const style = {
+            border: '1px solid #ccc',
+            boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+            ':hover': {
+                border: '1px solid #aaa',
+                boxShadow: '0 4px 15px 0 rgba(0,0,0, .25',
+                cursos: 'pointer'
+            }
+        }
+    
+    return (
+    <div className="Car" style={style} >
+        <h3>Car Name: {this.props.name}</h3>
+        <p>Year: <strong>{this.props.year}</strong></p>
+        
+        <input 
+        type="text " 
+        onChange={this.props.onChangeName} 
+        value={this.props.name}
+        className={inputClasses.join(' ')}
+        />
+    
+        <button onClick={this.props.onDelete}>Удалить</button>
+    
+    </div>
+    )
 
-    <button onClick={props.onDelete}>Удалить</button>
-
-</div>
-)
+        
+    }
 }
-
 
 
 
